effa6e48feed2afd5f41d52303da12a56f2c6e9e
asunafy <asunafy@126.com>
2022-10-08 22:55:34 +0800

    Create random.h

---
+++ PAT/random.h
@@ -0,0 +1,25 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#define RANDOM_DLL
+
+#ifdef RANDOM_DLL
+#define RANDOM_API __declspec(dllexport)			
+#else
+#define RANDOM_API __declspec(dllimport)			
+#endif 
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+int RANDOM_API RAND(int max, int min, int n);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif
+Footer
+© 2022 GitHub, Inc.
